6a86ee90407e3501f3afb22a621c8557860b45b2
Seke Nikola <sekenikola@gmail.com>
2019-03-18 09:52:28 +0100

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {
+  findByTestAttr
+} from '../utils/index'
+import {
+  shallow
+} from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Enzyme from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter(),
+  disableLifecycleMethods: true
 });
+
+const setUp = () => {
+  const component = shallow( < App / > );
+  return component;
+};
+
+describe('App Component', () => {
+
+  let component = setUp()
+  it('Should render without error', () => {
+    const appComponent = findByTestAttr(component, 'component-app');
+    expect(appComponent.length).toBe(1)
+  });
+
+  it('Should render Button', () => {
+    const button = findByTestAttr(component, 'increment-button');
+    expect(button.length).toBe(1)
+  });
+
+  it('Should render Counter', () => {
+    const counterRender = findByTestAttr(component, 'counter-render');
+    expect(counterRender.length).toBe(1)
+  });
+
+  it('Should  starts with 0', () => {
+
+  });
+
+  it('Click Should increment Counter', () => {
+
+  })
+})
